cb51c0c3942ea839e37d935ecec28bdd6ca9f820
simsim314 <simsim314@gmail.com>
2013-07-26 20:20:09 +0300

    Matrix test added

--- STLManagerTest/TestMatrix.cs
+++ STLManagerTest/TestMatrix.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using MatrixOperator;
 
 namespace STLManagerTest
 {
@@ -12,7 +13,14 @@ namespace STLManagerTest
         [Test]
         public void TransferFunds()
         {
-            Assert.AreEqual(250m, 250m);
+            // solving system of linear equations ... A * x = b
+            Matrix A = Matrix.Parse("1 0\r\n1 1"); // \r\n is newline in textbox
+            Matrix b = Matrix.Parse("-1\r\n-3");
+            Matrix x = A.SolveWith(b);
+
+            Assert.AreEqual(-1, x[0,0], "failed");
+            Assert.AreEqual(-2, x[1,0], "failed");
+
         }
     }
 }
