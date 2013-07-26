e5a843f45fbea6579bc264f85ad51e0139cf0b32
simsim314 <simsim314@gmail.com>
2013-07-26 14:47:27 +0300

    Z intersector added

--- STLManager/LineSegment.cs
+++ STLManager/LineSegment.cs
@@ -10,10 +10,19 @@ namespace STLManager
         private Vertex2D _left;
         private Vertex2D _right;
 
-        public LineSegment(Vertex2D left, Vertex2D right)
+        public LineSegment(Vertex2D v1, Vertex2D v2)
         {
-            _left = left;
-            _right = right;
+            if (v1.X < v2.X)
+            {
+                _left = v1;
+                _right = v2;
+            }
+            else
+            {
+                _left = v2;
+                _right = v1;
+
+            }
         }
     }
 }
--- STLManager/STLDocumentManager.cs
+++ STLManager/STLDocumentManager.cs
@@ -10,22 +10,70 @@ namespace STLManager
     {
         STLDocument _stl;
 
-        List<Facet> _minZs;
-        List<Facet> _maxZs;
-
         public STLDocumentManager(STLDocument stl)
         {
             _stl = stl;
         }
 
-        public void InitForZCutter()
-        {
 
+        private decimal MinZ(Facet f)
+        {
+            return f.Vertices.Min(vert => vert.Z);
         }
 
-        private decimal minZ(Facet f)
+        private decimal MaxZ(Facet f)
         {
             return f.Vertices.Max(vert => vert.Z);
+        }
+
+        private List<Facet> AllFacetsIntersectZ(decimal z)
+        {
+            return _stl.Facets.FindAll(f => MaxZ(f) >= z).FindAll(f => MinZ(f) <= z);
+        }
+
+        private List<LineSegment> IntersectWithZ(decimal z)
+        {
+            List<LineSegment> result = new List<LineSegment>();
+            AllFacetsIntersectZ(z).ForEach(f => result.Add(IntersectWithZ(f, z)));
+            return result;
+        }
+
+        private LineSegment IntersectWithZ(Facet f, decimal z)
+        {
+            int numOnZ = f.Vertices.Count(v => v.Z == z);
+
+            if (numOnZ > 1)
+                throw new Exception("Too many vertices on Z, add epsilon to z");
+
+            List<Vertex2D> vertices = new List<Vertex2D>();
+
+            for (int i = 0; i < f.Vertices.Count; i++)
+            {
+                var curVert = f.Vertices[i];
+                var nextVert = f.Vertices[(i + 1) % f.Vertices.Count];
+
+                if (z.CompareTo(curVert.Z) == z.CompareTo(nextVert.Z))
+                    continue;
+
+                decimal totalZ = Math.Abs(curVert.Z - nextVert.Z);
+                decimal curVertFactor = 1 - (Math.Abs(curVert.Z - z) / totalZ);
+                decimal nextVertFactor = 1 - (Math.Abs(nextVert.Z - z) / totalZ);
+
+                Vertex2D v2 = new Vertex2D();
+                v2.X = curVertFactor * curVert.X + nextVertFactor * nextVert.X;
+                v2.Y = curVertFactor * curVert.Y + nextVertFactor * nextVert.Y;
+
+                vertices.Add(v2);
+            }
+
+            if(vertices.Count != 2 && vertices.Count != 0)
+                throw new Exception("Too many vertices intersect Z, add epsilon to z");
+
+            if (vertices.Count == 0)
+                return null;
+
+            return new LineSegment(vertices[0], vertices[1]);
+
         }
     }
 }
