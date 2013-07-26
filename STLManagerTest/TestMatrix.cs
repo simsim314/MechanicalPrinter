f1efbc8db1cce9d9eb7b357bd51715739f5f1c37
simsim314 <simsim314@gmail.com>
2013-07-26 19:16:44 +0300

    tester added
    Matrix added

---
+++ STLManagerTest/TestMatrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace STLManagerTest
+{
+    [TestFixture]
+    class TestMatrix
+    {
+        [Test]
+        public void TransferFunds()
+        {
+            Assert.AreEqual(250m, 250m);
+        }
+    }
+}
